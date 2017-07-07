9c494e3b6575cf1601e2051371fbc8a58f01874d
Daniel Thompson <danthwa@gmail.com>
2017-07-07 11:18:17 -0700

    (not) working build

--- cardGame/ViewController.swift
+++ cardGame/ViewController.swift
@@ -7,35 +7,123 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate {
+    
+    
+    
     
     var mainDeck: Deck?
     var player1: Player?
     @IBOutlet weak var cardArea: UIStackView!
+    
+    var appDelegate: AppDelegate!
+    
+    let playerName = UIDevice.current.name
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainDeck = Deck()
-        player1 = Player(name: "James", toDraw: 1, deck: mainDeck!)
-        let card1 = player1?.hand[0]
         
-        let newCardView = CardViewController(frame: CGRect(), card: card1!)
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.mpcHandler.setupPeerWithDisplayName(displayName: UIDevice.current.name)
+        appDelegate.mpcHandler.setupSession()
+        appDelegate.mpcHandler.advertiseSelf(advertise: true)
         
-        cardArea.addSubview(newCardView)
+        NotificationCenter.default.addObserver(self, selector: #selector(peerChangedStateWIthNotification), name: NSNotification.Name(rawValue: "MPC_DidChangeStateNotification"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleReceivedDataWithNotification), name: NSNotification.Name(rawValue: "MPC_DidReceiveDataNotification"), object: nil)
         
     }
+    
+    
+    
+    
 
+    @IBAction func connectWithPlayer(_ sender: UIButton) {
+        if appDelegate.mpcHandler.session != nil {
+            appDelegate.mpcHandler.setupBrowser()
+            appDelegate.mpcHandler.browser.delegate = self
+            
+            self.present(appDelegate.mpcHandler.browser, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func drawCardPressed(_ sender: Any) {
+        let card = player1?.draw(from: mainDeck!)
+        let messageDict = ["card": card, "player": playerName]
+        
+        do {
+            
+            let messageData = try JSONSerialization.data(withJSONObject: messageDict, options: JSONSerialization.WritingOptions.prettyPrinted)
+            
+            try appDelegate.mpcHandler.session.send(messageData, toPeers: appDelegate.mpcHandler.session.connectedPeers, with: MCSessionSendDataMode.reliable)
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    func peerChangedStateWIthNotification(notification:NSNotification) {
+        let userInfo = NSDictionary(dictionary: notification.userInfo!)
+        
+        let state = userInfo.object(forKey: "state") as! Int
+        
+        if state != MCSessionState.connecting.rawValue {
+            self.navigationItem.title = "Connected"
+        }
+    }
+    
+    func handleReceivedDataWithNotification(notification: NSNotification){
+        
+        let userInfo = notification.userInfo! as Dictionary
+        let receivedData: Data = userInfo["data"] as! Data
+        
+        do {
+            
+            let message = try JSONSerialization.jsonObject(with: receivedData, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+            
+            let senderPeerID: MCPeerID = userInfo["peerID"] as! MCPeerID
+            let senderDisplayName = senderPeerID.displayName
+            
+            let card = message.object(forKey: "card")
+            let name = message.object(forKey: "player")
+            
+            print(card)
+            print(name)
+            print(senderDisplayName)
+            
+        } catch let jsonReadError {
+            print("jsonReadError: \(jsonReadError.localizedDescription)")
+        }
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        appDelegate.mpcHandler.browser.dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        appDelegate.mpcHandler.browser.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func drawCard(_ sender: Any) {
-        let card = player1?.draw(from: mainDeck!)
-        print(card)
-    }
-    
 
 }
 
